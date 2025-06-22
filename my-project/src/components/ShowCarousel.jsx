74226eaa6201ebfb2e133a0d9b52998351947b30
AnanyaKolar <kolar.ananya1@gmail.com>
2025-06-22 19:14:05 +0530

    done

--- my-project/src/components/ShowCarousel.jsx
+++ my-project/src/components/ShowCarousel.jsx
@@ -25,23 +25,6 @@ const ShowCarousel = ({ shows, hideScrollbar }) => {
           </div>
         ))}
       </div>
-      {/* Scroll buttons for desktop */}
-      <button
-        className="flex absolute left-0 top-1/2 -translate-y-1/2 bg-black/60 hover:bg-black text-white p-2 rounded-full z-10"
-        style={{ left: '0.5rem' }}
-        onClick={() => scroll('left')}
-        aria-label="Scroll left"
-      >
-        ◀
-      </button>
-      <button
-        className="flex absolute right-0 top-1/2 -translate-y-1/2 bg-black/60 hover:bg-black text-white p-2 rounded-full z-10"
-        style={{ right: '0.5rem' }}
-        onClick={() => scroll('right')}
-        aria-label="Scroll right"
-      >
-        ▶
-      </button>
     </div>
   );
 };
